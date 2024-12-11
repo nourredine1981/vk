6bc379642768e89cf83c7ab81d255d747e58f1bc
zaher <129608265+nourredine1981@users.noreply.github.com>
2024-12-11 07:54:51 +0100

    Update MyJavaClass.java

--- MyJavaClass.java
+++ MyJavaClass.java
@@ -1,3 +1,18 @@
+
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Bonjour depuis le code Java POO !";
+    }
+}
+
 public class MyJavaClass {
     public static void main(String[] args) {
         System.out.println("Bonjour depuis le code Java POO !");
