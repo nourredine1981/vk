e1639db6380f9c06a7d3c76982cf4d0025601f2e
zaher <129608265+nourredine1981@users.noreply.github.com>
2024-12-11 07:51:13 +0100

    Update MyJavaClass.java

--- MyJavaClass.java
+++ MyJavaClass.java
@@ -1,26 +1,5 @@
 public class MyJavaClass {
     public static void main(String[] args) {
-        if (args.length > 0) {
-            switch (args[0]) {
-                case "function1":
-                    function1();
-                    break;
-                case "function2":
-                    function2();
-                    break;
-                default:
-                    System.out.println("Function not found");
-            }
-        } else {
-            System.out.println("No function specified");
-        }
-    }
-
-    public static void function1() {
-        System.out.println("Function 1 called");
-    }
-
-    public static void function2() {
-        System.out.println("Function 2 called");
+        System.out.println("Bonjour depuis le code Java POO !");
     }
 }
